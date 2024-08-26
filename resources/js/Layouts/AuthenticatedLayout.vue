72bd38198e7e8aa7808367677e62189b1a5ae92f
tawfeeqtech <86443941+tawfeeqtech@users.noreply.github.com>
2024-08-26 12:02:43 +0300

    4 Share Roles and Permissions

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -6,8 +6,9 @@ import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
 import { Link } from '@inertiajs/vue3';
-
+import { usePermission } from '@/composables/permissions';
 const showingNavigationDropdown = ref(false);
+const { hasRole } = usePermission();
 </script>
 
 <template>
@@ -31,7 +32,8 @@ const showingNavigationDropdown = ref(false);
                                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
                                     Dashboard
                                 </NavLink>
-                                <NavLink :href="route('admin.index')" :active="route().current('admin.index')">
+                                <NavLink v-if="hasRole('admin')" :href="route('admin.index')"
+                                    :active="route().current('admin.index')">
                                     Admin
                                 </NavLink>
                             </div>
